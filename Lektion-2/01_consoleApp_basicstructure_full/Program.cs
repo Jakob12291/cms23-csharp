1b3d6f94b2435476f932987cb7597bc070f25249
Jakob12291 <hhalo007.jd@gmail.com>
2023-08-30 12:03:53 +0200

    Jakob

---
+++ Lektion-2/01_consoleApp_basicstructure_full/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _01_consoleApp_basicstructure_full
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
