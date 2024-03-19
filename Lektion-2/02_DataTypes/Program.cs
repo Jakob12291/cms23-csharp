f90e32d3748467750829fb6a71e0d879ba5f2cc2
Jakob12291 <44291708+Jakob12291@users.noreply.github.com>
2024-03-19 21:30:24 +0100

    Delete Lektion-2 directory

--- Lektion-2/02_DataTypes/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿Console.Write("Skriv ditt förnamn: ");
-string firstName = Console.ReadLine() ?? "";
-
-Console.Write("Skriv ditt efternamn: ");
-string lastName = Console.ReadLine() ?? string.Empty;
-
-Console.WriteLine($"Hej {firstName} {lastName}.");
-Console.ReadKey();
